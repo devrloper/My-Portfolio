96e0a28920fe5b7307f2be2d6276b7b8c66885d7
Your_Name <Your_Email>
2024-10-13 14:46:59 +0500

    Your commit message

---
+++ src/content_option.js
@@ -0,0 +1,160 @@
+const logotext = "Arooba Ahmad";
+const meta = {
+    title: "Arooba Ahmad",
+    description: "I’m Arooba Ahmad  Mern stack devloper",
+};
+
+const introdata = {
+    title: "I’m Arooba Ahmad",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I am a Mern Stack Developer",
+    },
+    description: "I’m a MERN stack developer with a focus on frontend development, skilled in building full-stack applications using HTML,CSS,Bootstrap,JavaScript, React JS, MongoDB, Express, and Node.js. My expertise lies in creating responsive, user-friendly interfaces while ensuring smooth integration with backend services.",
+    your_img_url: "https://i.pinimg.com/236x/1e/6a/2f/1e6a2fe6c7cc34e5c482cdc30aa40d7a.jpg",
+};
+
+const dataabout = {
+    title: "About my self",
+    aboutme: "I am a MERN stack developer with a strong focus on frontend development, specializing in creating responsive and user-friendly interfaces using React. While I have experience with full-stack development, including MongoDB, Express, and Node.js, my passion lies in crafting seamless and intuitive user experiences. I'm dedicated to continuous learning and delivering high-quality web applications that meet modern standards.",
+};
+// const worktimeline = [{
+//         jobtitle: "Designer of week",
+//         where: "YAdfi",
+//         date: "2020",
+//     },
+//     {
+//         jobtitle: "Designer of week",
+//         where: "Jamalya",
+//         date: "2019",
+//     },
+//     {
+//         jobtitle: "Designer of week",
+//         where: "ALquds",
+//         date: "2019",
+//     },
+// ];
+
+const skills = [{
+        name: "HTML",
+        value: 95,
+    },
+    {
+        name: "CSS",
+        value: 90,
+    },
+    {
+        name: "BootStrap",
+        value: 90,
+    },
+    {
+        name: "Javascript",
+        value: 85,
+    },
+    {
+        name: "React JS",
+        value: 90,
+    },
+    {
+        name: "MongoDB",
+        value: 85,
+    },
+    {
+        name: "Node JS",
+        value: 50,
+    },
+    {
+        name: "Word Press",
+        value: 70,
+    },
+    {
+        name: "Python",
+        value: 60,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Designing UI/UX involves creating intuitive interfaces that enhance user experience while ensuring aesthetic appeal. The goal is to balance functionality with creativity, making digital interactions seamless and engaging.",
+    },
+    {
+        title: "Mern Stack development",
+        description: "MERN stack development combines HTML,CSS,Bootstrap MongoDB, Express.js, React, and Node.js to create dynamic web applications using a unified JavaScript environment. It's efficient for building scalable and responsive full-stack applications.",
+    },
+    {
+        title: "Python",
+        description: " Skilled in beginner-level Python programming, with experience in writing basic scripts, working with data structures, and understanding core concepts like loops, functions, and conditionals. Familiar with libraries like NumPy and pandas for simple data manipulation",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Proficient in beginner level WordPress development with experience in creating and customizing websites using themes and plugins Familiar with basic content management and SEO optimization to enhance site performance.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://images.unsplash.com/flagged/photo-1590183030142-efad5a97612f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bmV0ZmxpeHxlbnwwfHwwfHx8MA%3D%3D",
+        description: "Netflix JavaScript Clone.",
+        link: "https://netflixclone-javascript.vercel.app/",
+    },
+    {
+        img: "https://img.freepik.com/free-vector/shiny-exterior-modern-skyline-building-background-with-reflection-effect_1017-44214.jpg",
+        description: " Beginers RealSate Website not responsive.",
+        link: "https://realest-git-main-aroobas-projects-13858e81.vercel.app/",
+    },
+    
+    {
+        img:"https://logowik.com/content/uploads/images/wellfound-new-20224679.logowik.com.webp",
+        description: "Welfound Webiste Clone.",
+        link: "https://welfoundclone-javascript-sz57.vercel.app/",
+    },
+    {
+        img: "https://media.istockphoto.com/id/652286150/photo/asia-business-concept-for-real-estate-and-corporate-construction-panoramic-modern-cityscape.jpg?s=612x612&w=0&k=20&c=u2NZHoQTxqJyvh4d2bCaIpWZr-eTwx9orzJh2vJ7Pio=",
+        description: "Construction Webiste.",
+        link: "https://construction1-umo3.vercel.app/",
+    },
+
+  
+    {
+        img: "https://images.unsplash.com/flagged/photo-1590183030142-efad5a97612f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bmV0ZmxpeHxlbnwwfHwwfHx8MA%3D%3D",
+
+        description: "Netflix ReactJS Clone.",
+        link: "https://netflixclonelive-reactjs.vercel.app/",
+    },
+    {
+        img: "https://w0.peakpx.com/wallpaper/106/535/HD-wallpaper-pc-gamer.jpg",
+        description: "Beginers Technology Website not responsive.",
+        link: "https://techno-html-css.vercel.app/",
+    },
+
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "aroobaahmad51@gmail.com",
+    YOUR_FONE: "0321-6580875",
+    description: "Have a project in mind or want to collaborate? I'd love to hear from you! Feel free to reach out with any inquiries or opportunities, and I'll get back to you as soon as possible. Let’s bring your ideas to life!",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/",
+    instagram: "https://www.instagram.com/?__pwa=1",
+    linkedin: "https://www.linkedin.com/in/arooba-ahmad-956359280?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    // worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
